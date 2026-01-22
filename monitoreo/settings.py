011e70965916736d76a72d626d28c4556f1cc5e4
Hector Uribe <dgic.h_uribe@juarez.gob.mx>
2026-01-22 12:28:10 -0700

    u

--- monitoreo/settings.py
+++ monitoreo/settings.py
@@ -86,6 +86,11 @@ DATABASES = {
     }
 }
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/panel/'
+LOGOUT_REDIRECT_URL = '/login/'  
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
