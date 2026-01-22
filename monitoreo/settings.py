e6678dbe1a4381898f08f55b3da5a02a4cc5bb33
Hector Uribe <dgic.h_uribe@juarez.gob.mx>
2026-01-22 09:36:26 -0700

    .

--- monitoreo/settings.py
+++ monitoreo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*n&goi$db!=5odf4zdf8tp0foey0*$dz+%#qli0(63c2vbj!y0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.236.0.33', '10.236.62.93']
+ALLOWED_HOSTS = ['10.236.0.33', '10.236.62.93', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['http://10.236.62.93:8082']
 
 # Application definition
